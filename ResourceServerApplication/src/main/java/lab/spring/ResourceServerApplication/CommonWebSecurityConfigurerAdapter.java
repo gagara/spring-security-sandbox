f3ed5e3659a271bd1b7bc3b74c3bf07208e52155
Viacheslav Gagara <viacheslavg@gmail.com>
2020-01-27 15:13:38 +0200

    RS: added CORS configuration

--- ResourceServerApplication/src/main/java/lab/spring/ResourceServerApplication/CommonWebSecurityConfigurerAdapter.java
+++ ResourceServerApplication/src/main/java/lab/spring/ResourceServerApplication/CommonWebSecurityConfigurerAdapter.java
@@ -1,18 +1,36 @@
 package lab.spring.ResourceServerApplication;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 public class CommonWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http //
+				.cors() //
+				.and() //
 				.antMatcher("/**") //
 				.authorizeRequests() //
 				.antMatchers("/public") //
 				.permitAll() //
 				.anyRequest() //
 				.authenticated();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://trustedhost"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
